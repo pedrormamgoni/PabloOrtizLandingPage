3a360111bfd7b24112b3ff7c37ec77b3d891e6e0
pedro <pedroaugustoribeiromangoni@gmail.com>
2025-07-28 21:43:24 -0300

    v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+        dinaboxDark: '#0F172A',
+        dinaboxPrimary: '#db652a',
+        dinaboxWhite: '#ffffff',
+      },
+    },
   },
   plugins: [],
 }
